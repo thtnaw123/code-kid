390b376ce585dd0fcbce704f60b0f4935f461d3f
Haile-08 <haile4cmd@gmail.com>
2023-04-13 14:45:59 +0300

    Setup the run and example canvas

--- src/Components/Animationspace/Animationspace.jsx
+++ src/Components/Animationspace/Animationspace.jsx
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./animationSpace.css";
 
 const Animationspace = () => {
+  const runCanvaRef = useRef(null);
+  const exampleCanvaRef = useRef(null);
+
+  useEffect(() => {
+    const render = () => {
+      const canvas = runCanvaRef.current;
+      const ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      requestAnimationFrame(render);
+    };
+    render();
+  }, []);
+  useEffect(() => {
+    const render = () => {
+      const canvas = exampleCanvaRef.current;
+      const ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      requestAnimationFrame(render);
+    };
+    render();
+  }, []);
   return (
     <>
       <div className="animationSpace">
-        <div className="result-animation">Result Animation</div>
-        <div className="example-animation">example Animation</div>
+        <div className="result-animation">
+          <canvas
+            className="run"
+            ref={runCanvaRef}
+            width="600px"
+            height="400px"
+          />
+        </div>
+        <div className="example-animation">
+          <canvas
+            className="example"
+            ref={exampleCanvaRef}
+            width="600px"
+            height="400px"
+          />
+        </div>
       </div>
     </>
   );
